528b2c08f4dd5af33686bbf0d465bf5fc2d64010
Jacob Waller <jaco@spotify.com>
2015-08-11 11:04:26 +0200

    Add inflating head

--- game/game.js
+++ game/game.js
@@ -5,19 +5,26 @@ window.onload = function() {
     create: create
   });
 
-  var bar;
+  var bar, body, head;
   var health = 100;
+  var value = 0;
 
   function preload () {
 
-    game.load.image('logo', 'phaser.png');
+    game.load.image('walken_body', 'resources/walken_body.png');
+    game.load.image('walken_head', 'resources/walken_head.png');
 
   }
 
   function create () {
 
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    body = game.add.sprite(400, 400, 'walken_body');
+    body.anchor.setTo(0.5, 0.5);
+    body.scale.setTo(0.5, 0.5);
+
+    head = game.add.sprite(288, 430, 'walken_head');
+    head.anchor.setTo(0.5, 0.9);
+    head.scale.setTo(0.5, 0.5);
 
     // keep the spacebar from propogating up to the browser
     game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
@@ -34,6 +41,12 @@ window.onload = function() {
   function onSpaceDown () {
     health -= 10;
     bar.scale.x = health / 100;
+
+    game.add.tween(head.scale)
+      .to({
+        x: head.scale.x + 0.05,
+        y: head.scale.y + 0.05
+      }, 200, Phaser.Easing.Back.Out, true);
   }
 
 };
